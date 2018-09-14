ab422dbdc584ef7c9507fc60a5b49740b532ea9a
JatinGoyal1307 <jt1481995@gmail.com>
2018-09-14 15:19:26 +0530

    Add files via upload

---
+++ cf743c.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class cf743c
+{
+	public static void main(String args[])throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		if(n==1)
+		{
+			System.out.println(-1);
+		}
+		else
+		{
+			System.out.println(n+" "+(n+1)+" "+(n*(n+1)));
+		}
+	}
+}
